ea0efbd92223f54db92931b869851faf15b23a81
yumoqing <yumoqing@gmail.com>
2019-06-21 14:27:37 +0800

    'r'

--- blocks/twistedplugin.py
+++
@@ -1,3 +0,0 @@
-from kivy.support import install_twisted_reactor
-install_twisted_reactor()
-
--- blocks/widgetExt/jsoncodeinput.py
+++
@@ -1,6 +0,0 @@
-from pygments.lexers.data import JsonLexer
-from kivy.uix.codeinput import CodeInput
-
-class JsonCodeInput(CodeInput):
-	def __init__(self,**kw):
-		super(JsonCodeInput,self).__init__(lexer = JsonLexer())
